5952693f5a85c7a1d68aa571009461442341aaa0
Claudia413 <cmjengelsman@gmail.com>
2017-06-29 18:11:19 +0200

    work in progress on algorithm

--- src/components/QuestionButton.js
+++ src/components/QuestionButton.js
@@ -25,13 +25,63 @@ export class QuestionButton extends PureComponent {
       return student.grades[student.grades.length -1] === 3;
     });
 
-    
-    // console.log(redStudents.map(function(student) { return student.grades})
-    // console.log(yellowStudents.map(function(student) { return student.name})
+    function percentageNum(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+
+    function shuffle(a) {
+      for (let i = a.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+      }
+    }
+
+    const chosenGroup = function() {
+      let pool = []
+
+      if (redStudents.length>0) {
+        let i = 0
+        while (i<50)
+        pool.push("r")
+        i++
+      }
+      if (yellowStudents.length>0) {
+        let i = 0
+        while (i<33)
+        pool.push("y")
+        i++
+      }
+      if (greenStudents.length>0) {
+        let i = 0
+        while (i<17)
+        pool.push("g")
+        i++
+      }
+       return pool[percentageNum(0, (pool.length-1))]
+    }
+
+    function chooseOneStudent(chosenGroup) {
+      switch(chosenGroup) {
+
+      case "r":
+        redStudents.shuffle
+        window.alert(redStudents[0].name)
+        break;
+      case "y":
+        yellowStudents.shuffle
+        window.alert(yellowStudents[0].name)
+        break;
+      case "g":
+        greenStudents.shuffle
+        window.alert(greenStudents[0].name)
+        break;
+      }
+    }
+
 
     return (
       <div>
-        <button>
+        <button onClick={ chooseOneStudent }>
           Choose a random student!
         </button>
 
